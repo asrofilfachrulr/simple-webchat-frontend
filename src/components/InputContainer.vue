cc5b6baa5a419d9598762789edf736c0e7df6400
anya <riidloa@gmail.com>
2023-11-17 18:00:55 +0700

    use enter to send message with wrapping inputs on form tag

--- src/components/InputContainer.vue
+++ src/components/InputContainer.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="w-full bg-neutral-100 flex p-2 gap-3">
-    <input
-      v-model="name"
-      class="flex-grow-[2] indent-4"
-      type="text"
-      placeholder="Name"
-    />
-    <input
-      v-model="content"
-      class="flex-grow-[6] indent-4"
-      type="text"
-      placeholder="Message"
-    />
-    <button
-      class="flex-grow-1 bg-green-600 p-1 px-3 text-white rounded-md active:bg-green-800 disabled:bg-green-300 disabled:cursor-not-allowed"
-      @click="sendMessageHandler"
-      :disabled="isEmptyOrWhiteSpaces(name) || isEmptyOrWhiteSpaces(content)"
-    >
-      Send
-    </button>
+  <div class="w-full bg-neutral-200">
+    <form @submit.prevent="sendMessageHandler" class="flex p-2 gap-3 h-full">
+      <input
+        v-model="name"
+        class="flex-grow-[2] indent-4"
+        type="text"
+        placeholder="Name"
+      />
+      <input
+        v-model="content"
+        class="flex-grow-[6] indent-4"
+        type="text"
+        placeholder="Message"
+      />
+      <button
+        class="flex-grow-1 bg-green-600 p-1 px-3 text-white rounded-md active:bg-green-800 disabled:bg-green-300 disabled:cursor-not-allowed"
+        type="submit"
+        @click="sendMessageHandler"
+        :disabled="isEmptyOrWhiteSpaces(name) || isEmptyOrWhiteSpaces(content)"
+      >
+        Send
+      </button>
+    </form>
   </div>
 </template>
 
